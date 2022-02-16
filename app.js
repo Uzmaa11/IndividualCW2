edb5a349638ff1259232d04063ccf1d1c546bad5
Uzma Shaikh <uzma1109@yahoo.com>
2022-02-16 13:55:56 +0400

    logger middleware

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const cors=("cors");
 // config Express.js
 app.use(express.json())
 
+// the 'logger' middleware
+app.use(function(req, res, next) {
+    console.log("Request IP: " + req.url);
+    console.log("Request date: " + new Date());
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+    });
 
 // Starts the app on port 3000 and display a message when it’s started
 app.listen(3000, function() {
